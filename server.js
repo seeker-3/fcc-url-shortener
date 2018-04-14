9e09330acd7a1e666c9fca19f2286ab6b8e4d663
Glitch (fcc-url-shortener-ab) <none>
2018-04-14 11:06:12 +0000

    db working

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express')
 const app = express()
 app.listen(process.env.PORT || 3000);
 
-const mongoUrl = 'mongodb://master:password@ds159926.mlab.com:59926/shortened-urls'
+const mLabUrl = 'mongodb://master:password@ds159926.mlab.com:59926/shortened-urls'
 
-MongoClient.connect(mongoUrl, function (err, db) {
+MongoClient.connect(mLabUrl, function (err, db) {
   if (err) console.log(err);
   else db.close();
 });
