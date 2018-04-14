dbc225a5f21f50aec6a3f2a7a472acaaa113c1fc
Glitch (fcc-url-shortener-ab) <none>
2018-04-14 11:45:26 +0000

    🐒🛍 Checkpoint ./server.js:855406/295

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ app.listen(process.env.PORT || 3000);
 
 const mLabUrl = 'mongodb://master:password@ds159926.mlab.com:59926/shortened-urls'
 
+function logObj(obj) {
+  for (let x in obj) {
+    console.log(x);
+  }
+}
+
+
 MongoClient.connect(mLabUrl, function (err, db) {
   if (err) console.log(err);
-  else db.close();
+  else {
+    log(db
+    //   .collection('chat').find().toArray(function(err, docs) {
+    //   console.log(JSON.stringify(docs));
+    // });
+    db.close();
+  }
 });
 
 app.get('/', (req, res) => 
@@ -20,5 +33,4 @@ app.get(/\/.+/, (req, res) => {
     original: null,
     shortened: null,
   });
-  res.end();
 });
