9df47e7425083bc45ec7849434e96524d1ef2c0f
Glitch (fcc-url-shortener-ab) <none>
2018-04-14 11:20:59 +0000

    added root page

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-<<<<<<< HEAD
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient
-=======
->>>>>>> f977617e12cdd3181a395adcdab6edee56f114eb
 const express = require('express')
 const app = express()
 app.listen(process.env.PORT || 3000);
 
-<<<<<<< HEAD
 const mLabUrl = 'mongodb://master:password@ds159926.mlab.com:59926/shortened-urls'
 
 MongoClient.connect(mLabUrl, function (err, db) {
   if (err) console.log(err);
   else db.close();
 });
-=======
->>>>>>> f977617e12cdd3181a395adcdab6edee56f114eb
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/root.html');
+});
