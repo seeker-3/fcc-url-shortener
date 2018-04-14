debab277b31c069e5593aceb5f9bec2f5be2b2a6
Glitch (fcc-url-shortener-ab) <none>
2018-04-14 11:01:28 +0000

    👙🍏 Checkpoint ./server.js:855406/1691

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
 const express = require('express')
 const app = express()
 app.listen(process.env.PORT || 3000);
 
+const mongoUrl = 'mongodb://master:password@ds159926.mlab.com:59926/shortened-urls'
+
+MongoClient.connect(mongoUrl, function (err, db) {
+  if (err) console.log(err);
+  else db.close();
+});
