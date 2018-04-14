0e6d689643e4994b2b30c2c3832c6c62ac64a696
Glitch (fcc-url-shortener-ab) <none>
2018-04-14 13:34:28 +0000

    🐮📫 Checkpoint ./server.js:855406/185

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ function logObj(obj) {
   for (let x in obj) {
     console.log(x);
   }
+  console.log(obj);
 }
 
 
 MongoClient.connect(mLabUrl, function (err, client) {
   if (err) console.log(err);
   else {
-    log(client.db());
+    logObj(client.db());
     //   .collection('chat').find().toArray(function(err, docs) {
     //   console.log(JSON.stringify(docs));
     // });
